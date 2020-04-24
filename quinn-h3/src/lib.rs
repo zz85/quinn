d964ac94fd05f710df67de5a35d58207425f5294
stammw <begue.jc@gmail.com>
2020-04-24 08:13:29 +0200

    H3: accept any type of error for body Polling
    This is perparing the HttpBody integration: Error::Body(HttpBody::Error)

--- quinn-h3/src/lib.rs
+++ quinn-h3/src/lib.rs
@@ -106,7 +106,7 @@ pub mod qpack;
 
 use err_derive::Error;
 use quinn::{ApplicationClose, ConnectionError, ReadError, WriteError};
-use std::io::ErrorKind;
+use std::{any::Any, fmt::Debug, io::ErrorKind};
 
 use proto::ErrorCode;
 
@@ -152,6 +152,9 @@ pub enum Error {
     /// The peer issued an HTTP/3 error code and an optional text description
     #[error(display = "Http error: {:?}", _0)]
     Http(HttpError, Option<String>),
+    /// Polling the issued body data yielded an error
+    #[error(display = "Polling body error: {:?}", _0)]
+    Body(Box<dyn Debug + Send + Sync>),
 }
 
 impl Error {
@@ -172,6 +175,13 @@ impl Error {
                 .and_then(|e| e.downcast_ref::<quinn_proto::ConnectionError>()),
             _ => None,
         }
+    }
+
+    pub(crate) fn body<T>(err: T) -> Self
+    where
+        T: Debug + Any + Send + Sync,
+    {
+        Self::Body(Box::new(err))
     }
 }
 
